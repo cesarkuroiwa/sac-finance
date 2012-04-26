b21b84a39b21d152a8cbae709a74a20a95f5df70
Your Name <signup@kuroiwa.com.br>
2012-04-26 18:03:48 -0300

    first version of sac.js

---
+++ sac.js
@@ -0,0 +1,22 @@
+function calculate()
+{
+	var result = document.getElementById("result");
+	var interest = document.getElementById("interest").value;
+	var value = document.getElementById("value").value;
+	var period = document.getElementById("period").value;
+	
+	interest = interest/100;
+	interest = interest/12;
+	var output = "";
+
+	var amort = 1.0*value/period;
+	var total = 0;
+	for (var i = 1; i <= period; i++) {
+		var payment = amort + value*interest;
+		value = value - amort;
+		total = total + payment;
+		output = output + i + " - R$ " + payment.toFixed(2) + "<br/>";
+	}
+	output = output + "Total: R$ " + total + "<br/>";
+	result.innerHTML = output;
+}
