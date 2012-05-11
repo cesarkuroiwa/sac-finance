1d655c073143107899f5156fea99e153573f4fcc
cesar <cesar@cesar.in.registro.br>
2012-05-11 19:03:02 -0300

    improvements

--- sac.js
+++ sac.js
@@ -4,7 +4,7 @@ function calculate()
 	var interest = document.getElementById("interest").value;
 	var value = document.getElementById("value").value;
 	var period = document.getElementById("period").value;
-	
+	var taxes = document.getElementById("taxes").value;
 	interest = interest/100;
 	interest = interest/12;
 	var output = "";
@@ -12,7 +12,7 @@ function calculate()
 	var amort = 1.0*value/period;
 	var total = 0;
 	for (var i = 1; i <= period; i++) {
-		var payment = amort + value*interest;
+		var payment = amort + value*interest + taxes*1.0;
 		value = value - amort;
 		total = total + payment;
 		output = output + i + " - R$ " + payment.toFixed(2) + "<br/>";
